9346c5ebb46f8d34e9f1be61efd1522718b4aea0
brunorodrigues0510-bit <brunorodrigues0510@outlook.com>
2026-04-14 22:48:32 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+-- Nessa query, utilizando JOIN pela coluna customer_id, trouxemos informações de pedido, status, cidade e estado como base, para a próxima query filtrar pedidos entregues em SP
 SELECT o.order_id, o.order_status, c.customer_city, c.customer_state
 FROM olist_orders o
 JOIN olist_customers c ON o.customer_id = c.customer_id
@@ -24,7 +25,7 @@ LIMIT 10;
 
 
 
-
+-- Utilizei essa query para descobrir as 10 cidades com maior montante de vendas totais em SP, considerando apenas pedidos entregues
 SELECT c.customer_city, sum (payment_value) as valor_total
 FROM olist_orders o
 JOIN olist_customers c ON o.customer_id = c.customer_id
@@ -75,3 +76,19 @@ WHERE o.order_status = 'delivered'
 GROUP BY c.customer_state
 HAVING media > 150
 ORDER BY media DESC
+
+
+
+-- Utilizei subqueries para descobrir de quais cidades foram os clientes que tiveram pagamento maior que a média geral da plataforma
+SELECT DISTINCT c.customer_city, c.customer_state
+FROM olist_customers c
+WHERE c.customer_id IN (
+    SELECT o.customer_id
+    FROM olist_orders o
+    JOIN olist_order_payments p ON o.order_id = p.order_id
+    WHERE p.payment_value > (
+        SELECT AVG(payment_value)
+        FROM olist_order_payments
+    )
+)
+LIMIT 10;  
